456fc2c3d0108b320cfef6e44aa1a4b5c682620e
Nhan <111131182+BNah1@users.noreply.github.com>
2024-05-17 03:11:05 +0700

    food app

---
+++ lib/page/favourite/favourite_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class FavouriteBody extends StatelessWidget {
+  const FavouriteBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('Favourite Page'));
+  }
+}
---
+++ lib/page/index.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:foody/config/const.dart';
+import 'package:foody/page/favourite/favourite_body.dart';
+import 'package:foody/page/home/home_body.dart';
+import 'package:foody/page/seen/seen_body.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomeBody(),
+    FavouriteBody(),
+    SeenBody(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: dColorMain,
+        centerTitle: true,
+        title: const Text('Foody'),
+      ),
+      body: _widgetOptions[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.food_bank),
+            label: 'Favourite',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.food_bank_rounded),
+            label: 'Seen',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue[100],
+        onTap: _onItemTapped,
+        backgroundColor: dColorMain,
+      ),
+    );
+  }
+}
