b077523bc45783e8f5cd2dea78e7e324d3407b9d
Nhan <111131182+BNah1@users.noreply.github.com>
2024-05-19 03:42:42 +0700

    food app

--- lib/config/const.dart
+++ lib/config/const.dart
@@ -1,3 +1,18 @@
 import 'package:flutter/material.dart';
 
 const dColorMain = Color(0xff0EAF89);
+const dColorIconButton = Colors.blue;
+const sizeIconButton = 24.0;
+const dColorFooterImage = Colors.white60;
+
+final styleTileItem = TextStyle(
+  color: Colors.black,
+  fontSize: 15,
+  fontWeight: FontWeight.bold,
+);
+
+final styleTileIcon = TextStyle(
+  color: Colors.black,
+  fontSize: 15,
+  fontWeight: FontWeight.bold,
+);
